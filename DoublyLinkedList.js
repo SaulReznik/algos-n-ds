36f7c3740035f219e9b9302ea39ce2f83e067945
Robert Hovhannisyan <robert.hovhannisyan@picsart.com>
2022-05-29 20:16:38 +0400

    DLL done

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -6,7 +6,7 @@ class Node {
   }
 }
 
-class SinglyLinkedList {
+class DoublyLinkedList {
   constructor(val) {
     this.length = 0;
     this.head = null;
@@ -56,4 +56,99 @@ class SinglyLinkedList {
     this.length--;
     return val;
   }
+
+  unshift(val) {
+    const node = new Node(val);
+      
+    if (!this.length) {
+        this.head = node;
+        this.tail = node;
+    } else {
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    const middle = Math.floor(this.length / 2);
+    let curr;
+    let target;
+
+    if (index > middle) {
+      curr = this.length - 1;
+      target = this.tail;
+      while(curr >= index) {
+        target = target.prev;
+        curr--;
+      }
+    } else {
+      curr = 0;
+      target = this.head;
+      while(curr !== index) {
+        target = target.next;
+        curr++;
+      }
+    }
+    return target;
+  }
+
+  set(index, value) {
+    const target = this.get(index);
+
+    if (!target) return false;
+
+    target.val = value;
+    return true;
+  }
+
+  insert(value, index) {
+    if (index < 0 || index >= this.length) return false;
+    if (!index) return !!this.unshift(value);
+    if (index === this.length) return !!this.push(value);
+    const target = this.get(index);
+    const node = new Node(value);
+    node.prev = target.prev;
+    node.next = target;
+    target.prev = node;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (!index) return !!this.shift();
+    if (index === this.length) return !!this.pop();
+    const target = this.get(index);
+    target.next.prev = target.prev;
+    target.prev.next = target.next;
+    target.prev = null;
+    target.next = null;
+    this.length--;
+    return target;
+  }
+
+  reverse() {
+    let temp = null;
+    let curr = this.head;
+    this.head = this.tail;
+    this.tail = curr;
+
+    while(curr !== null) {
+      temp = curr.prev;
+      curr.prev = curr.next;
+      curr.next = temp;
+      curr = curr.prev;
+    }
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+
+list.push(5).push(10).push(15).push(20);
+console.log(list.reverse());
