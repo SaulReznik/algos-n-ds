42193070dc6c646d56745b7373e310bc9f294b2d
SaulReznik <robhov97@gmail.com>
2022-12-21 23:59:37 +0400

    structured with directories

---
+++ dynamicProgramming/fib.js
@@ -0,0 +1,18 @@
+function fibMemo(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  const res = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+  memo[n] = res;
+  return res;
+};
+
+function fibTable(n) {
+  if (n <= 2) return 1;
+  const arr = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
+
+console.log(fibTable(1000));
--- recursion/recursiveSome.js
+++ recursion/recursiveSome.js
@@ -6,14 +6,14 @@ function someRecursive(arr, cb) {
     return someRecursive(arr, cb);
 };
 
-// Here's the course solution, mine is better
+// Here's the course solution, mine was mutating the original array
 function someRecursive(array, callback) {
     if (array.length === 0) return false;
     if (callback(array[0])) return true;
     return someRecursive(array.slice(1),callback);
 }
 
-console.log(someRecursive([1,2,3,4], isOdd)) // true
+console.log(someRecursive(arr, isOdd)) // true
 console.log(someRecursive([4,6,8,9], isOdd)) // true
 console.log(someRecursive([4,6,8], isOdd)) // false
 console.log(someRecursive([4,6,8], val => val > 10)); // false
