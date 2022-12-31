686cb0bf1478e9eea7f2a581933f30d34fa4c5c8
SaulReznik <robhov97@gmail.com>
2022-12-31 17:35:12 +0400

    allConstruct

---
+++ dynamicProgramming/allConstruct.js
@@ -0,0 +1,23 @@
+const allConstruct = (target, wordBank, memo = {}) => {
+  if (memo.hasOwnProperty(target)) return memo[target]
+  if (target === '') return [[]];
+
+  const result = [];
+
+  for (let word of wordBank) {
+    if (target.startsWith(word)) {
+      const suffix = target.slice(word.length);
+      const suffixWays = allConstruct(suffix, wordBank, memo);
+      const targetWays = suffixWays.map(way => [word, ...way]);
+      result.push(...targetWays);
+    }
+  }
+
+  memo[target] = result;
+  return result;
+};
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
+console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']));
+console.log(allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
+// console.log(allConstruct('eeeeeeeeeeeeeeeeeeeee', ['e', 'ee', 'eee', 'eeee', 'eeeee']));
