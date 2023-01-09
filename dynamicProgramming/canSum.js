823b7bfa05398f5cfee14e764250403d0bd40c26
SaulReznik <robhov97@gmail.com>
2023-01-09 21:38:22 +0400

    canSum table

--- dynamicProgramming/canSum.js
+++ dynamicProgramming/canSum.js
@@ -1,3 +1,6 @@
+// You are given a target number and an array of coins
+// You should see if it's possible to get a target sum with the givven coins
+
 const canSum = (targetSum, numbers, memo = {}) => {
   if (memo.hasOwnProperty(targetSum)) return memo[targetSum];
   if (targetSum === 0) return true;
@@ -7,7 +10,6 @@ const canSum = (targetSum, numbers, memo = {}) => {
     const remainder = targetSum - num;
     if (canSum(remainder, numbers, memo)) {
       memo[targetSum] = true;
-      console.log(memo);
       return true;
     }
   }
@@ -21,3 +23,28 @@ console.log(canSum(7, [5, 3, 4, 7]));
 console.log(canSum(7, [2, 4]));
 console.log(canSum(8, [2, 3, 5]));
 console.log(canSum(300, [7, 14]));
+console.log('--------');
+const canSumTable = (targetSum, coins) => {
+  const table = Array(targetSum + 1).fill(false);
+  table[0] = true;
+
+  for (let i = 0; i < table.length; i++) {
+    const cell = table[i];
+
+    if (cell) {
+      for (let coin of coins) {
+        if (i + coin <= targetSum) {
+          table[i + coin] = true;
+        }
+      }
+    }
+  }
+
+  return table[targetSum];
+}
+
+console.log(canSumTable(7, [2, 3]));
+console.log(canSumTable(7, [5, 3, 4, 7]));
+console.log(canSumTable(7, [2, 4]));
+console.log(canSumTable(8, [2, 3, 5]));
+console.log(canSumTable(300, [7, 14]));
