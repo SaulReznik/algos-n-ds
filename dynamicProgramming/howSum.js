7dd06bd7b517a9d8e7f30b120627cc894b6672ba
SaulReznik <robhov97@gmail.com>
2023-01-11 22:07:46 +0400

    howSum tabulation

--- dynamicProgramming/howSum.js
+++ dynamicProgramming/howSum.js
@@ -1,3 +1,6 @@
+// You are given a target number and an array of coins
+// You should return the combination of coins that in sum will get a target sum
+
 const howSum = (targetSum, numbers, memo = {}) => {
   if (memo.hasOwnProperty(targetSum)) return memo[targetSum];
   if (targetSum === 0) return [];
@@ -21,3 +24,28 @@ console.log(howSum(7, [5, 3, 4, 7]));
 console.log(howSum(7, [2, 4]));
 console.log(howSum(8, [2, 3, 5]));
 console.log(howSum(300, [7, 14]));
+console.log('----------');
+const howSumTable = (targetSum, coins) => {
+  const table = Array(targetSum + 1).fill(null);
+  table[0] = [];
+
+  for (let i = 0; i < table.length; i++) {
+    const combinations = table[i];
+
+    if (combinations) {
+      for (let coin of coins) {
+        if (i + coin <= targetSum) {
+          table[i + coin] = [...table[i], coin];
+        }
+      }
+    }
+  }
+
+  return table[targetSum];
+}
+
+console.log(howSumTable(7, [2, 3]));
+console.log(howSumTable(7, [5, 3, 4]));
+console.log(howSumTable(7, [2, 4]));
+console.log(howSumTable(8, [2, 3, 5]));
+console.log(howSumTable(300, [7, 14]));
