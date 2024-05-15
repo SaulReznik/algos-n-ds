bd924a2698549fe7848ec58c5087cf2b6ddd2410
SaulReznik <robhov97@gmail.com>
2024-05-15 20:58:57 +0400

    Segment tree done

--- dataStructures/SegmentTree.js
+++ dataStructures/SegmentTree.js
@@ -21,9 +21,48 @@ class SegmentTree {
     this.left = null;
     this.right = null;
   }
+
+  updateValue(index, val) {
+    if (this.leftIndex === this.rightIndex) {
+      if (this.leftIndex === index) this.value = val;
+      return;
+    }
+
+    const mid = Math.floor((this.leftIndex + this.rightIndex) / 2);
+    if (index <= mid) {
+      this.left.updateValue(index, val);
+    } else {
+      this.right.updateValue(index, val);
+    }
+
+    this.value = this.left.value + this.right.value;
+  }
+
+  rangeQuery(left, right) {
+    if (this.leftIndex === left && this.rightIndex === right) {
+      return this.value;
+    };
+
+    const mid = Math.floor((this.leftIndex + this.rightIndex) / 2);
+
+    if (mid < left) {
+      return this.right.rangeQuery(left, right)
+    } else if (mid >= right) {
+      return this.left.rangeQuery(left, right)
+    } else {
+      return this.left.rangeQuery(left, mid) + this.right.rangeQuery(mid + 1, right);
+    }
+  }
 }
 
 const arr = [4, 12, 3, 0, 8]
 const tree = buildTree(arr, 0, arr.length - 1);
 
 console.log(tree);
+console.log('-------------');
+tree.updateValue(0, 99);
+console.log(tree);
+console.log('-------------');
+console.log(tree.rangeQuery(0, 4));
+console.log(tree.rangeQuery(1, 4));
+console.log(tree.rangeQuery(3, 4));
